12fd45f7eb974a3b5e869aaac7326f906ab1ff5e
Manuel Entrena <manuel.entrena@gmail.com>
2021-05-12 15:25:49 +0200

    Añadimos url HTTPS

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -2,11 +2,11 @@ const KEY = "b87cafc48925a9ed60ca20297f8d38ac";
 
 export const consultarAPI = async (ciudad, pais, { latitude, longitude }) => {
   if (latitude && longitude) {
-    const urlGEO = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
+    const urlGEO = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
     const respuestaGEO = await fetch(urlGEO);
     return await respuestaGEO.json();
   } else {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${KEY}`;
     const respuesta = await fetch(url);
     return await respuesta.json();
   }
