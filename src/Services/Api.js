f0d8d49386f2605e1bfebe2e5366c4778fb43fd9
Manuel Entrena <manuel.entrena@gmail.com>
2021-05-12 15:49:50 +0200

    Urls dinamicas

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,12 +1,24 @@
 const KEY = "b87cafc48925a9ed60ca20297f8d38ac";
+const urlGEO = "";
+const url = "";
 
 export const consultarAPI = async (ciudad, pais, { latitude, longitude }) => {
   if (latitude && longitude) {
-    const urlGEO = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
+    if (location.protocol === "http:") {
+      urlGEO = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
+    } else {
+      urlGEO = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
+    }
+
     const respuestaGEO = await fetch(urlGEO);
     return await respuestaGEO.json();
   } else {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${KEY}`;
+    if (location.protocol === "http:") {
+      url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${KEY}`;
+    } else {
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${KEY}`;
+    }
+
     const respuesta = await fetch(url);
     return await respuesta.json();
   }
